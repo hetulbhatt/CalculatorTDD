e92aff7de031953ad441598288feeb6728f625e1
Hetul Bhatt <hetul21@gmail.com>
2021-01-18 21:19:25 +0530

    1. Test for single number

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,9 @@
 public class Calculator {
     public int add(String numbers) {
-        return 0;
+        if(numbers == null || numbers.isEmpty()) {
+            return 0;
+        } else {
+            return Integer.parseInt(numbers);
+        }
     }
 }
--- TestCalculator.java
+++ TestCalculator.java
@@ -4,7 +4,12 @@ import static org.junit.Assert.*;
 public class TestCalculator {
     
     @Test
-    public void testAdd() {
+    public void add_emptyis0() {
         assertEquals(0, new Calculator().add(""));
+    }
+    
+    @Test
+    public void add_singleNumberisTheNumberItself() {
+        assertEquals(20, new Calculator().add("20"));
     }
 }
